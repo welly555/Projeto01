c6c56f538a30eda15e4352d334e1537d489f705f
welly555 <wellyngton.targino@aluno.uepb.edu.br>
2022-12-26 19:38:00 -0300

    views dinamicas

--- recipes/views.py
+++ recipes/views.py
@@ -6,3 +6,7 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'recipes/pages/home.html')
+
+
+def recipes(request, id):
+    return render(request, 'recipes/pages/home.html')
