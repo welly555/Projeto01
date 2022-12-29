9288e643b5374b13bf12e5c0262766d53ccaf135
welly555 <wellyngton.targino@aluno.uepb.edu.br>
2022-12-29 18:28:08 -0300

    injetando dados

--- recipes/views.py
+++ recipes/views.py
@@ -1,12 +1,17 @@
 
 from django.shortcuts import render
 
+from utils.recipes.factory import make_recipe
+
 # Create your views here.
 
 
 def home(request):
-    return render(request, 'recipes/pages/home.html')
+
+    context = {'recipes': [make_recipe() for _ in range(10)]}
+    return render(request, 'recipes/pages/home.html', context=context)
 
 
 def recipes(request, id):
-    return render(request, 'recipes/pages/recipe-view.html')
+    context = {'recipes': [make_recipe()]}
+    return render(request, 'recipes/pages/recipe-view.html', context=context)
