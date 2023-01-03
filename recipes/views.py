e0af74722f5192f9b2acdd52fbfa3666da40b2da
welly555 <wellyngton.targino@aluno.uepb.edu.br>
2023-01-03 18:42:33 -0300

    erro 404 no detailg

--- recipes/views.py
+++ recipes/views.py
@@ -1,5 +1,5 @@
 
-from django.shortcuts import get_list_or_404, render
+from django.shortcuts import get_list_or_404, get_object_or_404, render
 
 from recipes.models import Recipe
 
@@ -26,7 +26,6 @@ def category(request, category_id):
 
 
 def recipe(request, id):
-    recipe = Recipe.objects.filter(
-        pk=id, is_published=True).order_by('-id').first()
+    recipe = get_object_or_404(Recipe, pk=id, is_published=True)
     context = {'recipe': recipe, 'is_detail_page': True, }
     return render(request, 'recipes/pages/recipe-view.html', context=context)
