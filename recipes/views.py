b4e60868ec7b9b0e733b7eb1f15bd8a8eafa1b1e
welly555 <wellyngton.targino@aluno.uepb.edu.br>
2023-01-03 17:38:54 -0300

    adicionando erro 404

--- recipes/views.py
+++ recipes/views.py
@@ -1,5 +1,5 @@
 
-from django.shortcuts import render
+from django.shortcuts import get_list_or_404, render
 
 from recipes.models import Recipe
 from utils.recipes.factory import make_recipe
@@ -15,10 +15,12 @@ def home(request):
 
 
 def category(request, category_id):
-    recipes = Recipe.objects.filter(
-        category__id=category_id, is_published=True).order_by('-id')
+    recipes = get_list_or_404(
+        Recipe.objects.filter(
+            category__id=category_id, is_published=True).order_by('-id'))
 
-    context = {'recipes': recipes}
+    context = {'recipes': recipes,
+               'tittle': f'{recipes[0].category.name}  - Category |'}
     return render(request, 'recipes/pages/category.html', context=context)
 
 
