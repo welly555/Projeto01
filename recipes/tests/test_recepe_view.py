1742d3970968397e8060a89d58452f14736e230c
welly555 <wellyngton.targino@aluno.uepb.edu.br>
2023-01-05 18:34:31 -0300

    ass test home 200 and load template home

--- recipes/tests/test_recepe_view.py
+++ recipes/tests/test_recepe_view.py
@@ -16,3 +16,11 @@ class RecipeViewTest(TestCase):
     def test_recipe_recipe_view_function_acept(self):
         view = resolve(reverse('recipes:recipe', kwargs={'id': 1}))
         self.assertIs(view.func, views.recipe)
+
+    def test_recipe_home_view_return_200_ok(self):
+        response = self.client.get(reverse('recipes:home'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_recipe_home_view_load_template_ok(self):
+        response = self.client.get(reverse('recipes:home'))
+        self.assertTemplateUsed(response, 'recipes/pages/home.html')
