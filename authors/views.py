9ddc6d410d6b60d10beea654cff567b661dba0f2
welly555 <wellyngton.targino@aluno.uepb.edu.br>
2023-01-24 20:10:14 -0300

    add forms in template

--- authors/views.py
+++ authors/views.py
@@ -1,7 +1,26 @@
+from django.http import Http404
 from django.shortcuts import render
+
+from .forms import RegisterForm
 
 # Create your views here.
 
 
 def register_view(request):
-    return render(request, 'author/pages/register_view.html')
+    form = RegisterForm()
+    return render(request, 'author/pages/register_view.html',
+                  {
+                      'form': form,
+                  })
+
+
+def register_criate(request):
+    if not request.POST:
+        raise Http404()
+
+    form = RegisterForm(request.POST)
+
+    return render(request, 'author/pages/register_view.html',
+                  {
+                      'form': form,
+                  })
