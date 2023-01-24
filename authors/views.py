4f1f1ddd460585b7ef7a3c13eb85ead11ec0cdbc
welly555 <wellyngton.targino@aluno.uepb.edu.br>
2023-01-24 20:39:03 -0300

    update in metdho register

--- authors/views.py
+++ authors/views.py
@@ -1,5 +1,5 @@
 from django.http import Http404
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 
 from .forms import RegisterForm
 
@@ -7,20 +7,20 @@ from .forms import RegisterForm
 
 
 def register_view(request):
-    form = RegisterForm()
+    register_form_data = request.session.get('register_form_data', None)
+
+    form = RegisterForm(register_form_data)
     return render(request, 'author/pages/register_view.html',
                   {
                       'form': form,
                   })
 
 
-def register_criate(request):
+def register_create(request):
     if not request.POST:
         raise Http404()
+    POST = request.POST
+    request.session['register_form_data'] = POST
+    form = RegisterForm(POST)
 
-    form = RegisterForm(request.POST)
-
-    return render(request, 'author/pages/register_view.html',
-                  {
-                      'form': form,
-                  })
+    return redirect('authors:register')
