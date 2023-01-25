564d34b3e73964267227c192161b5dc63a8fa4be
welly555 <wellyngton.targino@aluno.uepb.edu.br>
2023-01-25 19:19:30 -0300

    add methods for attributes

--- authors/forms.py
+++ authors/forms.py
@@ -2,7 +2,46 @@ from django import forms
 from django.contrib.auth.models import User
 
 
+def add_attr(field, attr_name, attr_new_val):
+    existing = field.widget.attrs.get(attr_name, '')
+    field.widget.attrs[attr_name] = f'{existing} {attr_new_val}'.strip()
+
+
+def add_placeholder(field, placeholder_val):
+    add_attr(field, 'placeholder', placeholder_val)
+
+
 class RegisterForm(forms.ModelForm):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        add_placeholder(self.fields['username'], 'You username')
+        add_placeholder(self.fields['email'], 'You e-mail')
+        add_placeholder(self.fields['first_name'], 'Ex.: jhon')
+        add_placeholder(self.fields['last_name'], 'Ex.: doe')
+        add_attr(self.fields['username'], 'css', 'a-css-class')
+
+    password = forms.CharField(
+        required=True,
+        widget=forms.PasswordInput(attrs={
+            'placeholder': 'Your password'
+        }),
+        error_messages={
+            'required': 'password must not by empy'
+        },
+        help_text=(
+            'Password must have at least one uppercase letter, '
+            'one lowercase letter and one number. The length should be '
+            'at least 8 characters.'
+        )
+    )
+    password_confirmed = forms.CharField(
+        required=True,
+        widget=forms.PasswordInput(attrs={
+            'placeholder': 'Repeat your password'
+        })
+    )
+
     class Meta:
         model = User
         fields = [
