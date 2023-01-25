3fa2c93a1ffd125c318f659c9d8f58141d58bc0e
welly555 <wellyngton.targino@aluno.uepb.edu.br>
2023-01-25 18:41:47 -0300

    formatting forms

--- authors/forms.py
+++ authors/forms.py
@@ -12,3 +12,28 @@ class RegisterForm(forms.ModelForm):
             'email',
             'password'
         ]
+        labels = {
+            'first_name': 'First name',
+            'last_name': 'Last name',
+            'username': 'Username',
+            'email': 'E-mail',
+            'password': 'Password'
+        }
+        help_texts = {
+            'email': 'the e-mail must be valid'
+        }
+        error_messages = {
+            'username': {
+                'required': 'this field must not by empy'
+            }
+        }
+
+        widgets = {
+            'first_name': forms.TextInput(attrs={
+                'placeholder': 'Type your Firstname here',
+                'class': 'input text-input'
+            }),
+            'password': forms.PasswordInput(attrs={
+                'placeholder': 'Type your password here'
+            })
+        }
