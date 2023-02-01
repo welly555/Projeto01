fe33bdc065487e24fb74c0a2786dd64c50a3fae6
welly555 <wellyngton.targino@aluno.uepb.edu.br>
2023-02-01 18:44:46 -0300

    cryptography of password

--- authors/views.py
+++ authors/views.py
@@ -25,7 +25,9 @@ def register_create(request):
     form = RegisterForm(POST)
 
     if form.is_valid():
-        form.save()
+        user = form.save(commit=False)
+        user.set_password(user.password)
+        user.save()
         messages.success(request, 'Your user is created, pleas login')
         del (request.session['register_form_data'])
 
