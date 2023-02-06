81a88c4aa3672a4e68c787497823aaeb7c19e399
welly555 <wellyngton.targino@aluno.uepb.edu.br>
2023-02-06 18:59:40 -0300

    start logout function and update at login

--- authors/views.py
+++ authors/views.py
@@ -1,5 +1,6 @@
 from django.contrib import messages
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 from django.http import Http404
 from django.shortcuts import redirect, render
 from django.urls import reverse
@@ -33,6 +34,7 @@ def register_create(request):
         user.save()
         messages.success(request, 'Your user is created, pleas login')
         del (request.session['register_form_data'])
+        return redirect(reverse('authors:login'))
 
     return redirect('authors:register')
 
@@ -66,3 +68,9 @@ def login_create(request):
     else:
         messages.error(request, 'Invalid username or password')
     return redirect(login_url)
+
+
+@login_required(login_url='authors:login', redirect_field_name='next')
+def logout_view(request):
+    logout(request)
+    return redirect(reverse('authors:login'))
