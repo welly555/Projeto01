a1a6e8db79800663473c122ab49709f7c7c9472f
welly555 <wellyngton.targino@aluno.uepb.edu.br>
2023-02-10 19:40:55 -0300

    test login invalid credentals and datas invalid

--- tests/funcional_tests/authors/test_authors_login.py
+++ tests/funcional_tests/authors/test_authors_login.py
@@ -2,17 +2,15 @@ import pytest
 from django.contrib.auth.models import User
 from django.urls import reverse
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
 
 from .base import AuthorsBaseTest
 
 
 @pytest.mark.funcional_test
 class AuthorsLoginTest(AuthorsBaseTest):
-    def test_user_valid_data_con_login_successefully(self):
-        string_password = 'pass'
+    def create_form(self, username, password):
         user = User.objects.create_user(
-            username='my_user', password=string_password)
+            username=username, password=password)
 
         self.browser.get(self.live_server_url + reverse('authors:login'))
 
@@ -21,13 +19,54 @@ class AuthorsLoginTest(AuthorsBaseTest):
         password_field = self.get_by_placeholder(form, 'Type your password')
 
         username_field.send_keys(user.username)
-        password_field.send_keys(string_password)
+        password_field.send_keys(password)
+
+        form.submit()
+
+    def test_user_valid_data_con_login_successefully(self):
+        self.create_form('my_user', 'pass')
+
+        self.assertIn(
+            'Your are logged in with my_user.',
+            self.browser.find_element(By.TAG_NAME, 'body').text
+        )
+
+    def test_login_create_raise_404_if_not_POST_method(self):
+        self.browser.get(
+            self.live_server_url + reverse('authors:login_create')
+        )
+
+        self.assertIn(
+            'Not Found',
+            self.browser.find_element(By.TAG_NAME, 'body').text
+        )
+        self.sleep(5)
+
+    def test_user_invalid_username_or_password(self):
+        self.create_form(' ', ' ')
+
+        self.assertIn(
+            'Invalid username or password',
+            self.browser.find_element(By.TAG_NAME, 'body').text
+        )
+
+    def test_credentials_invalid(self):
+        string_password = 'pass'
+        user = User.objects.create_user(
+            username='My_user', password=string_password)
+
+        self.browser.get(self.live_server_url + reverse('authors:login'))
+
+        form = self.browser.find_element(By.CLASS_NAME, 'main-form')
+        username_field = self.get_by_placeholder(form, 'Type your username')
+        password_field = self.get_by_placeholder(form, 'Type your password')
+
+        username_field.send_keys('my_user1')
+        password_field.send_keys('aknsdka')
 
         form.submit()
 
         self.assertIn(
-            f'Your are logged in with {user.username}.',
+            'Invalid credentials',
             self.browser.find_element(By.TAG_NAME, 'body').text
         )
-
-        # self.sleep(10)
