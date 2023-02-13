04967e4f7baeefab566ae271c734fa47fbe86653
welly555 <wellyngton.targino@aluno.uepb.edu.br>
2023-02-13 19:07:48 -0300

    update admin area

--- recipes/admin.py
+++ recipes/admin.py
@@ -7,9 +7,23 @@ class CategoryAdmin(admin.ModelAdmin):
     ...
 
 
+@admin.register(Recipe)
 class RecipeAdmin(admin.ModelAdmin):
-    ...
+    list_display = ['id', 'tittle', 'created_at', 'is_published', 'author']
+    list_display_links = ['tittle', 'created_at',]
+    search_fields = [
+        'id', 'tittle', 'description', 'slug', 'preparation_steps'
+    ]
+    list_filter = [
+        'category', 'author', 'preparation_steps_is_html', 'is_published'
+    ]
+    list_per_page = 10
+    list_editable = 'is_published',
+    ordering = '-id',
+    prepopulated_fields = {
+        "slug": ('tittle',)
+    }
 
 
 admin.site.register(Category, CategoryAdmin)
-admin.site.register(Recipe, RecipeAdmin)
+# admin.site.register(Recipe, RecipeAdmin)
