1e009335fa12c652aa89261aa0c618221e87e4bb
welly555 <wellyngton.targino@aluno.uepb.edu.br>
2023-02-15 18:37:06 -0300

    update data recipe for the template

--- authors/views.py
+++ authors/views.py
@@ -116,8 +116,21 @@ def dashboard_recipe_edit(request, id):
 
     form = AuthorRecipeForm(
         data=request.POST or None,
+        files=request.FILES or None,
         instance=recipe
     )
+
+    if form.is_valid():
+        recipe = form.save(commit=False)
+
+        recipe.author = request.user
+        recipe.preparation_steps_is_html = False
+        recipe.is_published = False
+
+        recipe.save()
+        messages.success(request, 'Your recipe was successfully saved ')
+        return redirect(reverse('authors:dashboard_recipe_edit', args=(id,)))
+
     return render(
         request,
         'authors/pages/dashboard_recipe.html',
